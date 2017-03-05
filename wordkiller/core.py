5bb69e1b7bf23c790c9eb1736b1ef2c04ae9fdbc
MerryMercy <zhenglianmin96@163.com>
2017-03-06 01:58:19 +0800

    compress audio files into a zip file

--- wordkiller/core.py
+++ wordkiller/core.py
@@ -440,24 +440,23 @@ def playMP3(filename):
     import pymedia.muxer as muxer
     import pymedia.audio.acodec as acodec
     import pymedia.audio.sound as sound
+    import zipfile
 
-    f = open(filename, 'rb') 
-    data= f.read(10000)
+    if ('us' in filename):
+        f = zipfile.ZipFile('.\\audio\\uk.zip')
+        mp3 = f.read('uk/' + filename[filename.rfind('\\')+1:])
+    else:
+        f = zipfile.ZipFile('.\\audio\\us.zip')
+        mp3 = f.read('us/' + filename[filename.rfind('\\')+1:])
+
+    data = mp3[:1<<16]
     dm = muxer.Demuxer('mp3')
     frames = dm.parse(data)
-    dec = acodec.Decoder(dm.streams[ 0 ])
+    dec = acodec.Decoder(dm.streams[0])
     frame = frames[0]
-    r = dec.decode(frame[ 1 ])
+    r = dec.decode(frame[1])
     snd = sound.Output(r.sample_rate, r.channels, sound.AFMT_S16_LE)
     if r: snd.play(r.data)
-
-    while True:
-        data = f.read(512)
-        if len(data)>0:
-            r = dec.decode(data)
-            if r: snd.play(r.data)
-        else:
-            break
     return snd
 
 if __name__ == '__main__' :
